f83db0ece4da19d109d70e6397dda9f64ebd3683
Omar Davidson <omardavidson227@gmail.com>
2025-01-16 14:28:28 -0500

    Initial Commit

---
+++ MBBACouponApp/MBBACouponAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBBACouponAppApp.swift
+//  MBBACouponApp
+//
+//  Created by Omar Davidson II on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MBBACouponAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
