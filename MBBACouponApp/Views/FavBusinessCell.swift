826ba89aa9af5aacfeb1e4b93d358b21bb254b47
AaronPreston <atpreston89@gmail.com>
2025-03-14 13:52:48 -0400

    Added functionality to Favorite Profile button

--- MBBACouponApp/Views/FavBusinessCell.swift
+++ MBBACouponApp/Views/FavBusinessCell.swift
@@ -7,22 +7,55 @@
 
 import SwiftUI
 
-struct FavBusinessCell: View {
+struct BusinessSheet: View {
     @ObservedObject var bm = BusinessManager()
     var body: some View {
-        HStack(spacing: 45) {
-            ForEach(bm.favBusinesses) { business in
-                VStack {
-                    Image(systemName: "")
-                        .frame(width: 100, height: 100)
-                        .background(.blue)
-                        .foregroundColor(.white)
-                        .clipShape(.circle)
-                    Text(business.accName)
-                }
+        VStack {
+            BusinessView()
         }
     }
-        .padding()
+}
+
+struct FavBusinessCell: View {
+    @ObservedObject var bm = BusinessManager()
+    @State var showBusiness = false
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                Button {
+                    showBusiness.toggle()
+                } label: {
+                    Image(systemName: "")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .clipShape(.circle)
+                        .ignoresSafeArea(.all)
+                }
+                .sheet(isPresented: $showBusiness) {
+                    BusinessSheet()
+                }
+                .padding()
+                .frame(width: 150, height: 150)
+                .background(Circle().fill(.blue))
+                .clipShape(.circle)
+                .shadow(color:.gray, radius: 3, x: -2, y: 2)
+            }
+            
+            HStack(spacing: 45) {
+                ForEach(bm.favBusinesses) { business in
+                    VStack {
+                        Image(systemName: "")
+                            .frame(width: 100, height: 100)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .clipShape(.circle)
+                        Text(business.accName)
+                    }
+                }
+            }
+            .padding()
+        }
     }
 }
 
