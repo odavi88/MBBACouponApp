ab51ac681ce58bff7b4a53d16286ad7ccaefde79
stevenalexjr <stevenalexjr@aol.com>
2025-04-19 08:54:47 -0400

    pushing to remote for big git merge, firestore functionality, coupons under business pages

---
+++ MBBACouponApp/Views/AddCouponView.swift
@@ -0,0 +1,36 @@
+//
+//  AddNoteView.swift
+//  MBBACouponApp
+//
+//  Created by Steven Alexander on 4/17/25.
+//
+
+
+import SwiftUI
+
+struct AddNoteView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var firestoreManager: FirestoreManager
+    @State private var title = ""
+    @State private var content = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Note Details")) {
+                    TextField("Title", text: $title)
+                    TextField("Content", text: $content)
+                }
+                
+                Button("Save") {
+                    firestoreManager.addNote(title: title, content: content)
+                    presentationMode.wrappedValue.dismiss()
+                }
+            }
+            .navigationTitle("Add")
+            .navigationBarItems(trailing: Button("Cancel") {
+                presentationMode.wrappedValue.dismiss()
+            })
+        }
+    }
+}
